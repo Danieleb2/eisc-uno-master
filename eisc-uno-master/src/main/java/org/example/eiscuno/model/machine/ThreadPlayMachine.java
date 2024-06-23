587d090d0f7e61c1cb8d7ee7d0ce084c3e3c15c6
TeheranMauricio <mt0686@gmail.com>
2024-06-23 11:23:37 -0500

    Third commit

--- eisc-uno-master/src/main/java/org/example/eiscuno/model/machine/ThreadPlayMachine.java
+++ eisc-uno-master/src/main/java/org/example/eiscuno/model/machine/ThreadPlayMachine.java
@@ -4,6 +4,7 @@ import javafx.scene.image.ImageView;
 import org.example.eiscuno.model.Observer;
 import org.example.eiscuno.model.Subject;
 import org.example.eiscuno.model.card.Card;
+import org.example.eiscuno.model.deck.Deck;
 import org.example.eiscuno.model.player.Player;
 import org.example.eiscuno.model.table.Table;
 
@@ -38,34 +39,56 @@ public class ThreadPlayMachine extends Thread implements Subject {
                 }
                 putCardOnTheTable();
                 hasPlayerPlayed = false;
+                notifyObservers(); // Notificar observadores después de que la máquina ha jugado
             }
         }
     }
 
-    private void putCardOnTheTable() {
-        int index = (int) (Math.random() * machinePlayer.getCardsPlayer().size());
-        Card card = machinePlayer.getCard(index);
+    public void putCardOnTheTable() {
+        Card card = getValidCardToPlay();
 
-        System.out.println(card.getValue());
-        System.out.println(card.getColor());
-
-        if (this.table.getCurrentCardOnTheTable().getColor().equalsIgnoreCase(card.getColor())) {
+        if (card != null) {
             table.addCardOnTheTable(card);
             tableImageView.setImage(card.getImage());
-            machinePlayer.removeCard(index);
-        } else if (this.table.getCurrentCardOnTheTable().getValue().equalsIgnoreCase(card.getValue())) {
-            table.addCardOnTheTable(card);
-            tableImageView.setImage(card.getImage());
-            machinePlayer.removeCard(index);
+            machinePlayer.getCardsPlayer().remove(card);
+            System.out.println("The machine player has "+machinePlayer.getCardsPlayer().size()+" cards");
+            System.out.println("Se añadió " + tableImageView.getImage());
         } else {
-            System.out.println("No puede colocar esta carta");
+            Card drawnCard = new Deck().takeCard();
+            if (validCardToPlay(drawnCard)) {
+                table.addCardOnTheTable(drawnCard);
+                tableImageView.setImage(drawnCard.getImage());
+            } else {
+                machinePlayer.addCard(drawnCard);
+                System.out.println("The machine player has "+machinePlayer.getCardsPlayer().size()+" cards");
+            }
         }
+    }
 
-        notifyObservers();
+    private Card getValidCardToPlay() {
+        for (Card card : machinePlayer.getCardsPlayer()) {
+            if (validCardToPlay(card)) {
+                return card;
+            }
+        }
+        return null;
+    }
+
+    private boolean validCardToPlay(Card card) {
+        Card cardOnTable = table.getCurrentCardOnTheTable();
+        if (cardOnTable.getValue().contains("skip")) {
+            return false;
+        } else if (card.getColor().equals(cardOnTable.getColor()) || card.getValue().equals(cardOnTable.getValue()) || card.getColor().equals("NON_COLOR")) {
+            return true;
+        }
+        return false;
     }
 
     public void setHasPlayerPlayed(boolean hasPlayerPlayed) {
         this.hasPlayerPlayed = hasPlayerPlayed;
+    }
+    public boolean hasPlayerPlayed() {
+        return hasPlayerPlayed;
     }
 
     public void stopThread() {
